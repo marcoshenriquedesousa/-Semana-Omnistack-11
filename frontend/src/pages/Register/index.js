85778df1288a0ff5ee7c4d346031efdcc1119cb5
Marcos henrique de sousa santos <henriquesantos.uefa@gmail.com>
2020-03-27 23:17:06 -0300

    mobile da aplicação

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -33,7 +33,7 @@ export default function Register() {
 
         try{
             const response = await api.post('ongs', data);
-            alert(`Seu ID de acesso: ${response.dara.id}`);
+            alert(`Seu ID de acesso: ${response.data.id}`);
 
             history.push('/');
         } catch(err) {
